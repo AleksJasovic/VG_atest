c061233642fcc21f8409af046a9fff4cd565ea5a
Aleksandra Jasovic <aki.aki.jasovic@gmail.com>
2023-01-27 08:32:17 +0100

    Lucky 6 koji radi

--- test/l6Proba.js
+++ test/l6Proba.js
@@ -4,7 +4,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 const chrome = require('selenium-webdriver/chrome')
-
+const chai = require('chai')
  
 
 describe('L6 proba', function() {
@@ -16,42 +16,57 @@ describe('L6 proba', function() {
     vars = {}
   })
   afterEach(async function() {
-    await driver.quit();
+   //driver.quit();
   })
   it('L6 proba', async function() {
-    setTimeout(async function () {
-    await driver.get("https://test.exevirtual.com/lucky6/index.php?deviceType=desktop&userUuid=AutomaticTest&sessionUuid=1e35114f-f081-4e6e-bd89-fb30b334f88b&language=sr&clientName=c2629cff41143af94bcec66d52117d75c370fb22&ccy=din")
-    await driver.manage().window().setRect({ width: 1540, height: 819 })
-    await driver.wait(until.ableToSwitchToFrame(By.id("qup")), 1000)
-    await driver.switchTo().frame(0)
+    await driver.get("https://test.exevirtual.com/lucky6/app.php?deviceType=desktop&userUuid=AutomaticTest&sessionUuid=1e35114f-f081-4e6e-bd89-fb30b334f88b&language=sr&clientName=c2629cff41143af94bcec66d52117d75c370fb22&ccy=din")
+    await driver.manage().window().setRect({ width: 1540, height: 1500 });
+
+
+    //await driver.switchTo().frame(0)
     
-    await driver.findElement(By.id("qup")).click()
+   //await driver.wait(until.ableToSwitchToFrame(By.className("lopticeNav randomPick")), 1000);
+    await driver.findElement(By.className("dugmiciLevoBtn prvaKugla")).click();
+    
+
+    await driver.findElement(By.id("addBet")).click();
+   
+
+    
+    await driver.findElement(By.id("plus")).click();
+   
+
+    await driver.findElement(By.className("uplatiBtn")).click();  
+    setTimeout(async ()=>{
+      await driver.findElement(By.id("deleteFromTicketBtn")).click();
+
+    },3000)
+   
+
+
     // {
-      const elements = await driver.findElements(By.linkText("6"))
-      assert(elements.length)
-    // }
-    await driver.findElement(By.css(".randomOpen > .randMore:nth-child(1) > span")).click()
-    await driver.wait(until.elementIsEnabled(await driver.findElement(By.id("addBet"))), 5000)
-    await driver.findElement(By.id("addBet")).click()
+    //   const elements = await driver.findElements(By.linkText("6"))
+    //   assert(elements.length)
+    // // }
+    // await driver.findElement(By.css(".randomOpen > .randMore:nth-child(1) > span")).click()
+    // await driver.wait(until.elementIsEnabled(await driver.findElement(By.id("addBet"))), 5000)
+    // // {
+    //   const element2 = await driver.findElement(By.id("addBet"))
+    //   await driver.actions({ bridge: true }).moveToElement(element2).perform()
+    // // }
+    // // {
+    //   const element = await driver.findElement(By.CSS_SELECTOR, "body")
+    //   await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
+    // // }
+    // await driver.wait(until.elementLocated(By.css(".activeRace .additionalInfo")), 5000)
     // {
-      const element2 = await driver.findElement(By.id("addBet"))
-      await driver.actions({ bridge: true }).moveToElement(element2).perform()
+    //   const elements = await driver.findElements(By.css(".activeRace .additionalInfo"))
+    //   assert(elements.length)
     // }
     // {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
+    //   const element = await driver.findElement(By.id("displayLast"))
+    //   await driver.actions({ bridge: true }).moveToElement(element).perform()
     // }
-    await driver.wait(until.elementLocated(By.css(".activeRace .additionalInfo")), 5000)
-    {
-      const elements = await driver.findElements(By.css(".activeRace .additionalInfo"))
-      assert(elements.length)
-    }
-    await driver.findElement(By.css(".uplatiBtn > .ng-binding")).click()
-    {
-      const element = await driver.findElement(By.id("displayLast"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    await driver.findElement(By.id("deleteFromTicketBtn")).click()
-  },40000)
+    // await driver.findElement(By.id("deleteFromTicketBtn")).click()
 })
 })
